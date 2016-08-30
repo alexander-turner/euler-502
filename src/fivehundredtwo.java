142f14a5e362c3d0b0159bc536b34802c9cdd3c6
Alex Turner <turneral1@grinnell.edu>
2016-08-30 08:54:30 -0500

    Tightened up TODOs and code. Fixed error in checking whether the row index can be advanced. Cleaned up formatting and reduced clutter in Main() function.

--- src/fivehundredtwo.java
+++ src/fivehundredtwo.java
@@ -5,7 +5,7 @@ import java.util.List;
 /*
 Project Euler: Problem 502 - https://projecteuler.net/problem=502
  
-A block is defined as a rectangle with a height of 1 and an integer-valued length.
+"A block is defined as a rectangle with a height of 1 and an integer-valued length.
 Let a castle be a configuration of stacked blocks.
 
 Given a game grid that is w units wide and h units tall, a castle is generated according to the following rules:
@@ -19,7 +19,7 @@ Given a game grid that is w units wide and h units tall, a castle is generated a
 
 Let F(w, h) represent the number of valid castles, given grid parameters w and h.
 For example, F(4, 2) = 10, F(13, 10) = 3729050610636, F(10, 13) = 37959702514, and F(100, 100) mod 1 000 000 007 =
-841913936.
+841913936."
 
 Summary:
 The algorithm calculates the number of castles on a w*h grid which satisfy the above conditions.
